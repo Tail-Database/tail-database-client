11970a0b74b656ac6ef4c0417836f9e0107ee735
Freddie Coleman <f.coleman@hotmail.co.uk>
2022-11-10 14:39:54 +0000

    add blockchain state RPC

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 export { coin_name, hash_program, hex_to_program, match_cat_puzzle, uncurry } from './src/clvm';
 export { CATEGORIES, COIN_CREATE_CONDITION, MAGIC_SPEND } from './src/constants';
 export { parseTailRecords, validateTailRecord } from './src/data_validation';
+export { Blockchain, RpcResponse } from './src/datalayer/blockchain_rpc';
 export { Coin, GetCoinRecordResponse, GetCoinRecordsByParentIdsResponse, GetPuzzleAndSolutionResponse } from './src/datalayer/coin_rpc';
 export { DataLayer, InsertResponse, UpdateResponse, ValueResponse } from './src/datalayer/data_layer_rpc';
 export { Tail, TailRecord, TailSerializer } from './src/datalayer/models/tail';
